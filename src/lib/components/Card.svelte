a433a0ace070b142624835cadaf587ee037039d6
Claudson <claudsondouglas.13@gmail.com>
2025-04-05 15:13:38 -0300

    feat: enhance Card UX with improved layout

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,25 +1,61 @@
 <script lang="ts">
-	import formatSatoshisToBitcoins from "$lib/utils/formatSatoshisToBitcoins";
-	import formatUnixToDatetime from "$lib/utils/formatUnixToDatetime";
-	import getLocalizedValue from "$lib/utils/getLocalizedValue";
+	import formatSatoshisToBitcoins from '$lib/utils/formatSatoshisToBitcoins';
+	import formatUnixToDatetime from '$lib/utils/formatUnixToDatetime';
+	import getLocalizedValue from '$lib/utils/getLocalizedValue';
 
 	export let node, index;
 </script>
 
-<div class="bg-white p-5">
-    <div class="flex items-center gap-3">
-        <span
-        class="w-8 h-8 bg-gray-900 text-white rounded-full flex items-center justify-center"
-        >{index}</span
-        >
-        <h2 class="text-lg md:text-xl font-bold" data-field="alias">
-            {node.alias}
-        </h2>
+<div class="bg-white p-5 cardnode">
+	<div class="flex items-center justify-between gap-3">
+		<div class="flex items-center gap-3">
+			<span
+				class="flex h-8 w-8 items-center justify-center rounded-full bg-gray-900 text-white"
+				>{index}</span
+			>
+			<h2 class="text-lg font-bold md:text-xl" data-field="alias">
+				{node.alias}
+			</h2>
+		</div>
+		<div class="flex items-center gap-2 text-sm font-light">
+			<span class="hidden md:block">{getLocalizedValue(node.city)}</span>
+			{#if node.iso_code}
+				<img
+					src={`https://flagcdn.com/w40/${node.iso_code?.toLowerCase()}.webp`}
+					class="h-5 w-7 object-cover"
+					alt={node.iso_code}
+				/>
+			{/if}
+		</div>
+	</div>
+	<div class="mt-3">
+		<div>
+			<strong>Canais:</strong>
+			<span data-field="channels">{node.channels.toLocaleString('pt-BR', {})}</span>
+		</div>
+		<div class="flex items-center gap-1">
+			<strong>Capacidade:</strong>
+			{formatSatoshisToBitcoins(node.capacity)} BTC
+		</div>
+
+		<div class="flex gap-1">
+			<strong>Localização:</strong>
+			{!getLocalizedValue(node.city) && !getLocalizedValue(node.country) ? 'Não informado' : ''}
+			{getLocalizedValue(node.city)}
+			{getLocalizedValue(node.city) && '/'}
+			{getLocalizedValue(node.country)}
+		</div>
+
+        <div class="flex gap-1">
+			<strong>Visto pela primeira:</strong> {formatUnixToDatetime(node.firstSeen)}
+        </div>
+
+        <div class="flex gap-1">
+			<strong>Ultima atualização:</strong> {formatUnixToDatetime(node.updatedAt)}
+        </div>
+	</div>
+
+	<div class="bg-gray-100 truncate p-2 mt-5 text-sm">
+		<span class="flex-1 truncate">{node.publicKey}</span>
     </div>
-	channels: {node?.channels}<br/>
-	{ getLocalizedValue(node?.city) } / {getLocalizedValue(node?.country)}<br />
-	capacidade: {formatSatoshisToBitcoins(node?.capacity)} BTC<br />
-	Visto pela primeira vez em {formatUnixToDatetime(node?.firstSeen)}<br />
-	Atualizado em {node?.updatedAt}<br/>
-    {node?.publicKey}<br />
 </div>
