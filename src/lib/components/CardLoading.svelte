c7bc57caa63d80032297563d084735f7569f2f6f
Claudson <claudsondouglas.13@gmail.com>
2025-04-05 21:49:06 -0300

    chore: fix dark shimmer

--- src/lib/components/CardLoading.svelte
+++ src/lib/components/CardLoading.svelte
@@ -3,35 +3,3 @@
 	<div class="animation-shimer h-[6.5em]"></div>
 	<div class="animation-shimer h-[2.3em]"></div>
 </div>
-
-<style>
-	.animation-shimer {
-		background: linear-gradient(-45deg, #eee 40%, #fafafa 50%, #eee 60%);
-		background-size: 300%;
-		background-position-x: 100%;
-		animation: shimmer 1s infinite linear;
-	}
-
-	.animation-shimer div {
-		background: linear-gradient(-45deg, #d2d2d2 40%, #fafafa 50%, #d2d2d2 60%);
-		background-size: 300%;
-		background-position-x: 100%;
-		animation: shimmer 1s infinite linear;
-	}
-
-	.dark {
-		.animation-shimer {
-			background: linear-gradient(-45deg, #222222 40%, #444444 50%, #222222 60%);
-			background-size: 300%;
-			background-position-x: 100%;
-			animation: shimmer 1s infinite linear;
-		}
-
-		.animation-shimer div {
-			background: linear-gradient(-45deg, #333333 40%, #5a5a5a 50%, #333333 60%);
-			background-size: 300%;
-			background-position-x: 100%;
-			animation: shimmer 1s infinite linear;
-		}
-	}
-</style>
